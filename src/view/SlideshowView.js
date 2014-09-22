35092cf06dda3620141bd2d0492e18965995840a
Trollglodita <1trollglodita@gmail.com>
2014-09-22 04:26:59 -0300

    Creating and Showing the Slides

--- src/view/SlideshowView.js
+++ src/view/SlideshowView.js
@@ -28,12 +28,35 @@ define(function(require, exports, module) {
         this.mainNode = this.add(this.rootModifier);
 
         _createLightbox.call(this);
+        _createSlides.call(this);
     }
 
     function _createLightbox() {
         this.lightbox = new Lightbox(this.options.lightboxOpts);
         this.mainNode.add(this.lightbox);
     }
+
+    function _createSlides() {
+        this.slides = [];
+        this.currentIndex = 0;
+
+        for (var i = 0; i < this.options.data.length; i++) {
+            var slide = new SlideView({
+                size: this.options.size,
+                photoUrl: this.options.data[i]
+            });
+
+            this.slides.push(slide);
+        }
+
+         this.showCurrentSlide();
+    }
+
+    SlideshowView.prototype.showCurrentSlide = function() {
+        var slide = this.slides[this.currentIndex];
+        this.lightbox.show(slide);
+    };
+
 
     module.exports = SlideshowView;
 });
