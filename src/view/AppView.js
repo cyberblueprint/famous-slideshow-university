d8bae1c66ce2ee55a19aef2214de77d5520349fe
Trollglodita <1trollglodita@gmail.com>
2014-09-22 21:46:14 -0300

    Adding the Camera

--- src/view/AppView.js
+++ src/view/AppView.js
@@ -6,12 +6,19 @@ define(function(require, exports, module) {
 
     var SlideshowView = require('view/SlideshowView');
 
+    var ImageSurface = require('famous/surfaces/ImageSurface');
+
     AppView.prototype = Object.create(View.prototype);
     AppView.prototype.constructor = AppView;
 
     AppView.DEFAULT_OPTIONS = {
-        data: undefined
+        data: undefined,
+        cameraWidth: 0.6 * window.innerHeight
     };
+
+    AppView.DEFAULT_OPTIONS.slideWidth = 0.8 * AppView.DEFAULT_OPTIONS.cameraWidth;
+    AppView.DEFAULT_OPTIONS.slideHeight = AppView.DEFAULT_OPTIONS.slideWidth + 40;
+    AppView.DEFAULT_OPTIONS.slidePosition = 0.77 * AppView.DEFAULT_OPTIONS.cameraWidth;
 
     function AppView(options) {
         View.apply(this, arguments);
@@ -21,6 +28,25 @@ define(function(require, exports, module) {
         });
 
         this.add(slideshow);
+        _createCamera.call(this);
+    }
+
+    function _createCamera() {
+        var camera = new ImageSurface({
+            size: [this.options.cameraWidth, true],
+            content: 'img/camera.png',
+            properties: {
+                width: '100%'
+            }
+        });
+
+        var cameraModifier = new StateModifier({
+            origin: [0.5, 0],
+            align: [0.5, 0],
+            transform: Transform.behind
+        });
+
+        this.add(cameraModifier).add(camera);
     }
 
     module.exports = AppView;
